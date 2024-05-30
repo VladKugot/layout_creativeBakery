6799b05ce5682b439c14b59b11d47bb8519f0651
Vlad <vladkygot@gmail.com>
2024-05-30 09:50:35 +0300

    make build

---
+++ index.ad7fd25b.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(":root");document.querySelector("#dark").addEventListener("click",()=>{event.preventDefault(),e.classList.toggle("dark")});
+//# sourceMappingURL=index.ad7fd25b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+let root = document.querySelector(":root");
+let button = document.querySelector("#dark");
+button.addEventListener("click", ()=>{
+    event.preventDefault();
+    root.classList.toggle("dark");
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
